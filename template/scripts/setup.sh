905469a0195d6a7f77095c9f8994da8477cb587b
DavideSegullo <davidesegullo@gmail.com>
2022-10-24 21:16:33 +0200

    build: :bug: fix ios build

--- template/scripts/setup.sh
+++ template/scripts/setup.sh
@@ -6,3 +6,7 @@ yarn
 npx rn-nodeify --install --hack
 find ${PWD}/node_modules/*/ -name "build.gradle" -type f | xargs perl -pi -e 's/\bcompile\b/implementation/g;'
 npx jetify
+
+# fix for ios duplicate symbols for architecture arm64 xcode: https://stackoverflow.com/a/68911887
+rm -rf node_modules/react-native-tcp/ios/CocoaAsyncSocket
+rm -rf node_modules/react-native-udp/ios/CocoaAsyncSocket
